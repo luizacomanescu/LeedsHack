baed4be16d2b6abe60a15c63f65e71d69be0115d
annabalotescu <annabalotescu@172.20.226.116>
2018-04-21 16:28:58 +0100

    Implemented Game.java and InitialScenario.java

--- Game.java
+++ Game.java
@@ -1,3 +1,48 @@
 public class Game {
 
+	int NoOfScenarios = 1;
+	int chosenScenario;
+	
+	public static void main(String[] args)
+	{
+		// make user choose a scenario and save in chosenScenario - fe
+		
+		String scenarioAsString = InitialScenerio.getInitialScenario(chosenScenario);
+		
+		// show user the initial scenario - fe
+		
+		String []questions = Questions.getQuestions(chosenScenario);
+		String []answers = Answers.getAnswers(chosenScenario);
+		
+		// show all questions to user with checkboxes - fe
+		boolean selectedQuestions = new boolean[9];
+		
+		// show yes/no answer for the questions selected by the user - fe
+		
+		// user answers the other remaining question with yes/no checkboxes
+		boolean userAnswerCorrect;
+		int score = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			if (selectedQuestions[i] == false)
+			{
+				// make user answer this question - fe
+				
+				
+				if (userAnswerCorrect == true)
+					score++;
+			}
+			
+		} // for
+		if (score >= 4)
+		{
+			// tell user has won - fe
+			
+			String finalScenarioAsString = FinalScenario.getFinalScenario(scenarioChosen);
+			// show user the final scenario - fe
+			
+		}
+		else 
+			//tell user lost - fe
+	}
 }
--- InitialScenario.java
+++ InitialScenario.java
@@ -1,7 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class InitialScenario {
-/*
-    public String getInitialScenario()
+
+    public String getInitialScenario(int chosenScenario)
     {
-      return " ";
-    }*/
+    	 String fileName = "/resources/initialscenario" + chosenScenario.toString(); 
+    	 BufferedReader br = new BufferedReader(new FileReader("fileName"));	 
+     StringBuilder sb = new StringBuilder();
+     String line = br.readLine();
+
+        while (line != null) {
+          sb.append(line);
+          sb.append("\n");
+          line = br.readLine();
+        }
+          return sb.toString();  
+     }     
 }
